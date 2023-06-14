cbe2cf9f441df88292984ff74ccf9fb75c06f0b8
Anthony Chiado <anthonychiado97@gmail.com>
2023-06-14 10:21:02 -0400

    Initial commit

--- technical_orientation.R
+++ technical_orientation.R
@@ -1,1 +1,1 @@
-comp_specs_data <- read.csv(file = "~wd/data/2023-06-13-survey.csv")
+comp_specs_data <- read.csv(file = "/Users/anthonychiado/Desktop/work/uva/technical_orientation/data/2023-06-13-survey.csv")
